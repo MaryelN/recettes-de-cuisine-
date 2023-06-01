e712debd655fe8be7106748139f14a3434d5c268
Maryel Navarro <maryel.navarromtz@gmai.com>
2023-06-01 13:53:57 +0200

    link of pages

--- templates/header.php
+++ templates/header.php
@@ -24,8 +24,8 @@
         </a>
 
       <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
-        <li><a href="#" class="nav-link px-2 link-secondary">Home</a></li>
-        <li><a href="#" class="nav-link px-2">Features</a></li>
+        <li><a href="/index.php" class="nav-link px-2 link-secondary">Accueil</a></li>
+        <li><a href="/recettes.php" class="nav-link px-2">Nos recettes</a></li>
         <li><a href="#" class="nav-link px-2">Pricing</a></li>
         <li><a href="#" class="nav-link px-2">FAQs</a></li>
         <li><a href="#" class="nav-link px-2">About</a></li>
--- templates/recipe_partial.php
+++ templates/recipe_partial.php
@@ -3,11 +3,10 @@
             <!-- Pour utiliser la constante avec notre boucle foreach -->
               <img src="<?=_RECIPES_IMG_PATH_.$recipe['image']?>" class="card-img-top" alt="image de <?=$recipe['title']?>">
               <div class="card-body">
-                <!-- Pour faire un echo directement dans la ligne HTML -->
-
                 <h5 class="card-title"><?=$recipe['title']?></h5>
                 <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
+                <!-- On done un id a notre href avec le $key de notre tableau -->
+                <a href="/recette.php?id=<?=$key; ?>" class="btn btn-primary">Lire recette</a>
               </div>
             </div>
           </div>
